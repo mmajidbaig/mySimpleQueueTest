364bb22dd7d00714c44a4b614a83c27a6b77b573
Muhammad Majid Baig <m_majidbaig@yahoo.com>
2025-01-27 01:40:22 +1300

    Final Refactor code

--- test/ctest_queue.C
+++ test/ctest_queue.C
@@ -43,22 +43,22 @@ void test_fill_queue() {
     
     struct QUEUE q;
     init_q(&q);
-    //display_q(&q);
+    
     TEST_ASSERT_TRUE(is_q_empty(&q));
     TEST_ASSERT_FALSE(put_q(10,&q));
     TEST_ASSERT_FALSE(put_q(20,&q));
-    TEST_ASSERT_FALSE(is_q_empty(&q));    
+    TEST_ASSERT_FALSE(is_q_empty(&q));  
+    display_q(&q); 
 }
 
 void test_de_queue() {
     
     struct QUEUE q;
-    init_q(&q);
     byte  data;
     TEST_ASSERT_FALSE(get_q(&q,&data));
-    //TEST_ASSERT_EQUAL_INT8(10,data);
-    //TEST_ASSERT_FALSE(get_q(&q,&data));
-    //TEST_ASSERT_EQUAL_INT8(20,data);
+    TEST_ASSERT_EQUAL_INT8(10,data);
+    TEST_ASSERT_FALSE(get_q(&q,&data));
+    TEST_ASSERT_EQUAL_INT8(20,data);
         
 }
 
@@ -77,36 +77,5 @@ int main() {
   	RUN_TEST(test_queue_initialization);
   	RUN_TEST(test_fill_queue);
   	RUN_TEST(test_de_queue);
-  	display_q(&q);
-    return UNITY_END();
-		
-    // Test adding elements to the queue
-    /*
-    printf("Adding elements to the queue:\n");
-    for (byte i = 1; i <= 5; i++) {
-        if (put_q(i, &q)==0) {
-            printf("Added %d\n", i);
-            } else {
-            printf("Queue is full, could not add %d\n", i);
-        }
-    }
-
-    display_q(&q);
-    
-
-    // Test removing elements from the queue
-    printf("\nRemoving elements from the queue:\n");
-    byte data;
-    for (int i = 0; i < 5; i++) {
-        if (get_q(&q, &data) == 0) {
-            printf("Removed %d\n", data);
-        } else {
-            printf("Queue is empty, could not remove element.\n");
-        }
-    }
-
-    display_q(&q);
-  
-    return 0;
-    */
-}
+  	return UNITY_END();
+		}
